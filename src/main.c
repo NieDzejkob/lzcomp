00573fe0d859372db9d7463dced789b7dbb5f7fe
Jakub Kądziołka <kuba@kadziolka.net>
2021-08-07 15:04:46 +0200

    Remove inferior compression methods

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@ int main (int argc, char ** argv) {
     } else
       write_commands_and_padding_to_textfile(options.output, commands, size, file_buffer, original_size - remainder, remainder);
   } else {
-    commands = compress(file_buffer, &size, options.method);
+    commands = compress(file_buffer, &size);
     (options.mode ? write_commands_to_textfile : write_commands_to_file)(options.output, commands, size, file_buffer, options.alignment);
   }
   free(file_buffer);
@@ -25,7 +25,7 @@ int main (int argc, char ** argv) {
   return 0;
 }
 
-struct command * compress (const unsigned char * data, unsigned short * size, unsigned method) {
+struct command * compress (const unsigned char * data, unsigned short * size) {
   unsigned char * bitflipped = malloc(*size);
   unsigned current;
   for (current = 0; current < *size; current ++) bitflipped[current] = bit_flipping_table[data[current]];
